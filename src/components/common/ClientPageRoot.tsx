2b83bb277c7731eda1f8af216e63a308c8d0c860
[dyad] <acarolinaambrizi@gmail.com>
2026-06-05 17:46:55 -0300

    [dyad] wrote 1 file(s)

---
+++ src/components/common/ClientPageRoot.tsx
@@ -0,0 +1,21 @@
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { supabase } from "@/integrations/supabase/client";
+
+export default function ClientPageRoot() {
+  const router = useRouter();
+  
+  useEffect(() => {
+    const session = supabase.auth.getSession();
+    session.then(({ data: { session } }) => {
+      if (session) {
+        router.push("/home");
+      } else {
+        router.push("/login");
+      }
+    });
+  }, [router]);
+
+  return null;
+}
